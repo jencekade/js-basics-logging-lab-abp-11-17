059f4ac2d7e75bcb455c5edae24e8092c62cc1fe
jencekade <gaephuoc@gmail.com>
2017-11-06 09:04:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,4 @@ const driver = 'bob';
 console.log("this code was called");
 console.log(driver);
 
-//var logger = function (driver, "is the driver variable name"){
-  return driver + ' '+ 'is the driver variable name';
-
-
 console.log(driver + " " + "is the driver variable name");
